111ea10897a8bd6b35dccb5832bf52360e0c3f28
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-16 16:25:53 +0100

    Add translator service type

---
+++ NucleicAcidConverter/Service/TranslatorService.cs
@@ -0,0 +1,18 @@
+﻿using NucleicAcidConverter.Model;
+
+namespace NucleicAcidConverter.Service;
+
+public class TranslatorService : ITranslatorService
+{
+    private readonly IAminoAcidProviderService _aminoAcidProvider;
+
+    public TranslatorService(IAminoAcidProviderService aminoAcidProvider)
+    {
+        _aminoAcidProvider = aminoAcidProvider;
+    }
+
+    public IEnumerable<AminoAcid> TranslateSequence(Sequence sequence)
+    {
+        return sequence.Select(_aminoAcidProvider.GetAminoAcid);
+    }
+}
