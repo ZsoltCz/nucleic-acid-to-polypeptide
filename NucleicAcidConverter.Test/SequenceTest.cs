8de9efbc4d2e272b4e4dab371ba5426d9c5309e9
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-17 13:19:13 +0100

    Add unit test for sequence

---
+++ NucleicAcidConverter.Test/SequenceTest.cs
@@ -0,0 +1,22 @@
+﻿using NucleicAcidConverter.Model;
+
+namespace NucleicAcidConverter.Test;
+
+[TestFixture]
+public class SequenceTest
+{
+    [Test]
+    public void EnumeratesCorrectly()
+    {
+        var sequence = new Sequence("AATAATG", 1);
+
+        var actual = sequence.ToList();
+        var expected = new List<string>()
+        {
+            "ATA",
+            "ATG"
+        };
+        
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+}
