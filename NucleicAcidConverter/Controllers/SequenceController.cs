564fe14f09272903815de31b3e33157324a0ec3d
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-18 17:04:56 +0100

    Add sequence controller

---
+++ NucleicAcidConverter/Controllers/SequenceController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NucleicAcidConverter.Model;
+using NucleicAcidConverter.Service;
+
+namespace NucleicAcidConverter.Controllers;
+
+[ApiController]
+public class SequenceController : ControllerBase
+{
+    private readonly ITranslatorService _translatorService;
+
+    public SequenceController(ITranslatorService translatorService)
+    {
+        _translatorService = translatorService;
+    }
+
+    [HttpGet]
+    [Route("translate")]
+    public IEnumerable<AminoAcid> Translate(Sequence sequence)
+    {
+        return _translatorService.TranslateSequence(sequence);
+    }
+}
