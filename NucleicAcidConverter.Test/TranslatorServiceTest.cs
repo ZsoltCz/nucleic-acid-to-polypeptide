a8f59ea117f692728308c32bc8299743031f14e2
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-24 14:33:57 +0100

    Initialize translator service test class

--- NucleicAcidConverter.Test/TranslatorServiceTest.cs
+++ NucleicAcidConverter.Test/TranslatorServiceTest.cs
@@ -1,7 +1,15 @@
-﻿namespace NucleicAcidConverter.Test;
+﻿using NucleicAcidConverter.Service;
+
+namespace NucleicAcidConverter.Test;
 
 [TestFixture]
 public class TranslatorServiceTest
 {
-    
+    private ITranslatorService _translatorService;
+
+    public TranslatorServiceTest(ITranslatorService translatorService)
+    {
+        IAminoAcidProviderService aminoAcidProvider = new AminoAcidProviderService();
+        _translatorService = new TranslatorService(aminoAcidProvider);
+    }
 }
