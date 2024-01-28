6a16b808d50baa7779c1a5994e4e65fb9d984a7c
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-28 12:57:20 +0100

    Add amino acid provider test logic

--- NucleicAcidConverter.Test/AminoAcidProviderServiceTest.cs
+++ NucleicAcidConverter.Test/AminoAcidProviderServiceTest.cs
@@ -1,7 +1,28 @@
-﻿namespace NucleicAcidConverter.Test;
+﻿using NucleicAcidConverter.Service;
+
+namespace NucleicAcidConverter.Test;
 
 [TestFixture]
 public class AminoAcidProviderServiceTest
 {
-    
+    private IAminoAcidProviderService _aminoAcidProviderService;
+
+    private static readonly object[] TestCases =
+    {
+        
+    };
+
+    [OneTimeSetUp]
+    public void Init()
+    {
+        _aminoAcidProviderService = new AminoAcidProviderService();
+    }
+
+    [TestCaseSource(nameof(TestCases))]
+    public void GetsCorrectAminoAcid(string codon, string expectedResult)
+    {
+        var actual = _aminoAcidProviderService.GetAminoAcid(codon).ThreeLetterSymbol;
+        
+        Assert.That(actual, Is.EqualTo(expectedResult));
+    }
 }
