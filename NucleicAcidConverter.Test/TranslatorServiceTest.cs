fd064b87450317693cec0467fa08e0920c7b5ec4
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-29 13:36:24 +0100

    Add testing logic to translator service test

--- NucleicAcidConverter.Test/TranslatorServiceTest.cs
+++ NucleicAcidConverter.Test/TranslatorServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using NucleicAcidConverter.Service;
+﻿using NucleicAcidConverter.Model;
+using NucleicAcidConverter.Service;
 
 namespace NucleicAcidConverter.Test;
 
@@ -6,11 +7,24 @@ namespace NucleicAcidConverter.Test;
 public class TranslatorServiceTest
 {
     private ITranslatorService _translatorService;
-    
+
+    private static readonly object[] TranslationTestCases =
+    {
+        
+    };
+
     [OneTimeSetUp]
     public void Init()
     {
         var aminoAcidProviderService = new AminoAcidProviderService();
         _translatorService = new TranslatorService(aminoAcidProviderService);
+    }
+
+    [TestCaseSource(nameof(TranslationTestCases))]
+    public void TranslatesSequenceCorrectly(Sequence sequence, string[] expectedResult)
+    {
+        var actual = _translatorService.TranslateSequence(sequence).Select(aminoAcid => aminoAcid.OneLetterSymbol);
+        
+        Assert.That(actual, Is.EqualTo(expectedResult));
     }
 }
