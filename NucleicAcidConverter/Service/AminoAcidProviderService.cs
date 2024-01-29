624e08127beb26128ca239f9281c2b433caa0b9d
ZsoltCz <zsoltcz2019@gmail.com>
2024-01-29 14:12:20 +0100

    Remove redundant type specification

--- NucleicAcidConverter/Service/AminoAcidProviderService.cs
+++ NucleicAcidConverter/Service/AminoAcidProviderService.cs
@@ -4,29 +4,29 @@ namespace NucleicAcidConverter.Service;
 
 public class AminoAcidProviderService : IAminoAcidProviderService
 {
-    private static readonly IDictionary<AminoAcid, string[]> AminoAcidCodonMap = new Dictionary<AminoAcid, string[]>()
+    private static readonly IDictionary<AminoAcid, string[]> AminoAcidCodonMap = new Dictionary<AminoAcid, string[]>
     {
-        { new AminoAcid("Alanine", "Ala", "A"), new string[] { "GCU", "GCC", "GCA", "GCG" } },
-        { new AminoAcid("Arginine", "Arg", "R"), new string[] { "CGU", "CGC", "CGA", "CGG", "AGA", "AGG" } },
-        { new AminoAcid("Asparagine", "Asn", "N"), new string[] { "AAU", "AAC" } },
-        { new AminoAcid("Aspartic Acid", "Asp", "D"), new string[] { "GAU", "GAC" } },
-        { new AminoAcid("Cysteine", "Cys", "C"), new string[] { "UGU", "UGC" } },
-        { new AminoAcid("Glutamic Acid", "Glu", "E"), new string[] { "GAA", "GAG" } },
-        { new AminoAcid("Glutamine", "Gln", "Q"), new string[] { "CAA", "CAG" } },
-        { new AminoAcid("Glycine", "Gly", "G"), new string[] { "GGU", "GGC", "GGA", "GGG" } },
-        { new AminoAcid("Histidine", "His", "H"), new string[] { "CAU", "CAC" } },
-        { new AminoAcid("Isoleucine", "Ile", "I"), new string[] { "AUU", "AUC", "AUA" } },
-        { new AminoAcid("Leucine", "Leu", "L"), new string[] { "UUA", "UUG", "CUU", "CUC", "CUA", "CUG" } },
-        { new AminoAcid("Lysine", "Lys", "K"), new string[] { "AAA", "AAG" } },
-        { new AminoAcid("Methionine", "Met", "M"), new string[] { "AUG" } },
-        { new AminoAcid("Phenylalanine", "Phe", "F"), new string[] { "UUU", "UUC" } },
-        { new AminoAcid("Proline", "Pro", "P"), new string[] { "CCU", "CCC", "CCA", "CCG" } },
-        { new AminoAcid("Serine", "Ser", "S"), new string[] { "UCU", "UCC", "UCA", "UCG", "AGU", "AGC" } },
-        { new AminoAcid("Threonine", "Thr", "T"), new string[] { "ACU", "ACC", "ACA", "ACG" } },
-        { new AminoAcid("Tryptophan", "Trp", "W"), new string[] { "UGG" } },
-        { new AminoAcid("Tyrosine", "Tyr", "Y"), new string[] { "UAU", "UAC" } },
-        { new AminoAcid("Valine", "Val", "V"), new string[] { "GUU", "GUC", "GUA", "GUG" } },
-        { new AminoAcid("Stop Codon", "Stop", "*"), new string[] { "UAA", "UAG", "UGA" } }
+        { new AminoAcid("Alanine", "Ala", "A"), new[] { "GCU", "GCC", "GCA", "GCG" } },
+        { new AminoAcid("Arginine", "Arg", "R"), new[] { "CGU", "CGC", "CGA", "CGG", "AGA", "AGG" } },
+        { new AminoAcid("Asparagine", "Asn", "N"), new[] { "AAU", "AAC" } },
+        { new AminoAcid("Aspartic Acid", "Asp", "D"), new[] { "GAU", "GAC" } },
+        { new AminoAcid("Cysteine", "Cys", "C"), new[] { "UGU", "UGC" } },
+        { new AminoAcid("Glutamic Acid", "Glu", "E"), new[] { "GAA", "GAG" } },
+        { new AminoAcid("Glutamine", "Gln", "Q"), new[] { "CAA", "CAG" } },
+        { new AminoAcid("Glycine", "Gly", "G"), new[] { "GGU", "GGC", "GGA", "GGG" } },
+        { new AminoAcid("Histidine", "His", "H"), new[] { "CAU", "CAC" } },
+        { new AminoAcid("Isoleucine", "Ile", "I"), new[] { "AUU", "AUC", "AUA" } },
+        { new AminoAcid("Leucine", "Leu", "L"), new[] { "UUA", "UUG", "CUU", "CUC", "CUA", "CUG" } },
+        { new AminoAcid("Lysine", "Lys", "K"), new[] { "AAA", "AAG" } },
+        { new AminoAcid("Methionine", "Met", "M"), new[] { "AUG" } },
+        { new AminoAcid("Phenylalanine", "Phe", "F"), new[] { "UUU", "UUC" } },
+        { new AminoAcid("Proline", "Pro", "P"), new[] { "CCU", "CCC", "CCA", "CCG" } },
+        { new AminoAcid("Serine", "Ser", "S"), new[] { "UCU", "UCC", "UCA", "UCG", "AGU", "AGC" } },
+        { new AminoAcid("Threonine", "Thr", "T"), new[] { "ACU", "ACC", "ACA", "ACG" } },
+        { new AminoAcid("Tryptophan", "Trp", "W"), new[] { "UGG" } },
+        { new AminoAcid("Tyrosine", "Tyr", "Y"), new[] { "UAU", "UAC" } },
+        { new AminoAcid("Valine", "Val", "V"), new[] { "GUU", "GUC", "GUA", "GUG" } },
+        { new AminoAcid("Stop Codon", "Stop", "*"), new[] { "UAA", "UAG", "UGA" } }
     };
 
     public AminoAcid GetAminoAcid(string codon)
