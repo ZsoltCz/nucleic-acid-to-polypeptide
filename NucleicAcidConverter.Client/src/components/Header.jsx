852d2007e61b18f14e34f56b86b48126bf168251
ZsoltCz <zsoltcz2019@gmail.com>
2024-02-06 14:57:58 +0100

    Add icon to header component

--- NucleicAcidConverter.Client/src/components/Header.jsx
+++ NucleicAcidConverter.Client/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import { AppBar } from '@mui/material'
+import { AppBar, Container, Typography } from '@mui/material';
+import ScienceOutlinedIcon from "@mui/icons-material/ScienceOutlined";
 
-const Header = () => {
+export default function Header() {
   return (
     <AppBar>
-      
+      <Container>
+        <ScienceOutlinedIcon />
+        <Typography>
+          Nucleic Acid To Polypeptide Converter
+        </Typography>
+      </Container>
     </AppBar>
-  )
-}
-
-export default Header
+  );
+};
