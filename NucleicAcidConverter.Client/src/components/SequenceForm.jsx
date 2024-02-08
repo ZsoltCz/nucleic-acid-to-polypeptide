3caba314ff7a91b2d435598585e5e2cc362a5f5b
ZsoltCz <zsoltcz2019@gmail.com>
2024-02-08 15:49:37 +0100

    Introduce states to sequence form component

--- NucleicAcidConverter.Client/src/components/SequenceForm.jsx
+++ NucleicAcidConverter.Client/src/components/SequenceForm.jsx
@@ -6,12 +6,43 @@ import {
   Select,
   TextField,
 } from "@mui/material";
+import { useState } from "react";
 
 const readingFrames = [0, 1, 2];
 
-export default function SequenceForm() {
+export default function SequenceForm({ setTranslationResult }) {
+
+  const [sequence, setSequence] = useState({
+    nucleotideSequence: "",
+    readingFrame: 0
+  });
+
+  const handleChange = (event) => {
+    setSequence(prevSequence => {
+      return {
+        ...prevSequence,
+        [event.target.name]: event.target.value
+      };
+    });
+  };
+
+  const formStyleTemp = {
+    marginTop: "20px"
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:5007/translate?NucleotideSequence=${sequence.nucleotideSequence}&ReadingFrame=${sequence.readingFrame}`);
+      const aminoAcids = await response.json();
+      setTranslationResult(aminoAcids);
+    } catch (error) {
+      console.error(error);
+    };
+  };
+
   return (
-    <form>
+    <form style={formStyleTemp} onSubmit={handleSubmit}>
       <FormControl>
         <TextField
           label="DNA or RNA sequence"
@@ -19,18 +50,21 @@ export default function SequenceForm() {
           required
           multiline
           rows="4"
+          name="nucleotideSequence"
+          onChange={handleChange}
+          value={sequence.nucleotideSequence}
         />
       </FormControl>
       <FormControl>
         <InputLabel htmlFor="reading-frame">Reading frame</InputLabel>
-        <Select>
+        <Select defaultValue={0} name="readingFrame" onChange={handleChange}>
           {readingFrames.map((readingFrame) => (
-            <MenuItem value={readingFrame}>{readingFrame + 1}</MenuItem>
+            <MenuItem key={readingFrame} value={readingFrame}>{readingFrame + 1}</MenuItem>
           ))}
         </Select>
       </FormControl>
       <FormControl>
-        <Button variant="outlined">
+        <Button type="submit" variant="outlined">
           Translate
         </Button>
       </FormControl>
