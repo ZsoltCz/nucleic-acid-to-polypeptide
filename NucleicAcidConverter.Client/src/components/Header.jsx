5a36b3ba56339ccb4faaba90f71593f99471fe8f
ZsoltCz <zsoltcz2019@gmail.com>
2024-02-12 09:56:21 +0100

    Improve header component

--- NucleicAcidConverter.Client/src/components/Header.jsx
+++ NucleicAcidConverter.Client/src/components/Header.jsx
@@ -1,15 +1,15 @@
-import { AppBar, Container, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import ScienceOutlinedIcon from "@mui/icons-material/ScienceOutlined";
 
 export default function Header() {
   return (
     <AppBar position="static">
-      <Container>
-        <ScienceOutlinedIcon />
-        <Typography display="inline">
+      <Toolbar>
+        <ScienceOutlinedIcon fontSize="large" sx={{ mr: 2 }} />
+        <Typography variant="h5">
           Nucleic Acid To Polypeptide Converter
         </Typography>
-      </Container>
+      </Toolbar>
     </AppBar>
   );
 };
