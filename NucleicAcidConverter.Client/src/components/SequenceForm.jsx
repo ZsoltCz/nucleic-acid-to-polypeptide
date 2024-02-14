b7ec0b0d44381649cc0ae2cdcf57cb71f25e201e
ZsoltCz <zsoltcz2019@gmail.com>
2024-02-14 13:33:35 +0100

    Center page content with container component

--- NucleicAcidConverter.Client/src/components/SequenceForm.jsx
+++ NucleicAcidConverter.Client/src/components/SequenceForm.jsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Container,
   FormControl,
   Grid,
   InputLabel,
@@ -70,70 +71,72 @@ export default function SequenceForm({ setTranslationResult, displayedProperty, 
   };
 
   return (
-    <form style={formStyleTemp} onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <Grid container direction="column" spacing={2} item xs={6}>
-          <Grid item xs={6}>
-            <FormControl fullWidth>
-              <TextField
-                label="DNA or RNA sequence"
-                id="nucleotide-sequence"
-                required
-                multiline
-                rows="4"
-                name="nucleotideSequence"
-                onChange={handleSequenceChange}
-                value={sequence.nucleotideSequence}
-              />
-            </FormControl>
+    <Container>
+      <form style={formStyleTemp} onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid container direction="column" spacing={2} item xs={6}>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <TextField
+                  label="DNA or RNA sequence"
+                  id="nucleotide-sequence"
+                  required
+                  multiline
+                  rows="4"
+                  name="nucleotideSequence"
+                  onChange={handleSequenceChange}
+                  value={sequence.nucleotideSequence}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={3}>
+              <FormControl fullWidth>
+                <Button type="submit" variant="outlined">
+                  Translate
+                </Button>
+              </FormControl>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <FormControl fullWidth>
-              <Button type="submit" variant="outlined">
-                Translate
-              </Button>
-            </FormControl>
-          </Grid>
-        </Grid>
-        <Grid container direction="column" spacing={2} item xs={6}>
-          <Grid item xs={1}>
-            <FormControl fullWidth>
-              <InputLabel id="reading-frame-label">Reading frame</InputLabel>
-              <Select
-                labelId="reading-frame-label"
-                value={sequence.readingFrame}
-                label="Reading frame"
-                name="readingFrame"
-                onChange={handleSequenceChange}
-              >
-                {readingFrames.map((readingFrame) => (
-                  <MenuItem key={readingFrame} value={readingFrame}>
-                    {readingFrame + 1}
+          <Grid container direction="column" spacing={2} item xs={6}>
+            <Grid item xs={1}>
+              <FormControl fullWidth>
+                <InputLabel id="reading-frame-label">Reading frame</InputLabel>
+                <Select
+                  labelId="reading-frame-label"
+                  value={sequence.readingFrame}
+                  label="Reading frame"
+                  name="readingFrame"
+                  onChange={handleSequenceChange}
+                >
+                  {readingFrames.map((readingFrame) => (
+                    <MenuItem key={readingFrame} value={readingFrame}>
+                      {readingFrame + 1}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={1}>
+              <FormControl fullWidth>
+                <InputLabel id="displayed-property-label">
+                  Amino acid display
+                </InputLabel>
+                <Select
+                  value={displayedProperty}
+                  labelId="displayed-property-label"
+                  onChange={handleDisplayChange}
+                >
+                  <MenuItem value="name">Name</MenuItem>
+                  <MenuItem value="threeLetterSymbol">
+                    Three letter symbol
                   </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={1}>
-            <FormControl fullWidth>
-              <InputLabel id="displayed-property-label">
-                Amino acid display
-              </InputLabel>
-              <Select
-                value={displayedProperty}
-                labelId="displayed-property-label"
-                onChange={handleDisplayChange}
-              >
-                <MenuItem value="name">Name</MenuItem>
-                <MenuItem value="threeLetterSymbol">
-                  Three letter symbol
-                </MenuItem>
-                <MenuItem value="oneLetterSymbol">One letter symbol</MenuItem>
-              </Select>
-            </FormControl>
+                  <MenuItem value="oneLetterSymbol">One letter symbol</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </form>
+      </form>
+    </Container>
   );
 };
