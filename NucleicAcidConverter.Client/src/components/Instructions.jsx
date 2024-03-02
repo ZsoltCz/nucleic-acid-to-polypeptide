69f051bb6ba444efb07fd1826abfcf54c7fe8260
ZsoltCz <zsoltcz2019@gmail.com>
2024-03-02 11:46:52 +0100

    Improve instruction component's style

--- NucleicAcidConverter.Client/src/components/Instructions.jsx
+++ NucleicAcidConverter.Client/src/components/Instructions.jsx
@@ -3,10 +3,14 @@ import { Container, Typography } from "@mui/material";
 export default function Instructions() {
 
   return (
-    <Container>
-      <Typography>
+    <Container sx={{ marginTop: "20px" }}>
+      <Typography variant="h5" gutterBottom>
         Welcome to Nucleic Acid To Polypeptide Converter!
+      </Typography>
+      <Typography variant="h6" gutterBottom>
         Input a DNA or RNA sequence below, select a reading frame and press the translate button!
+      </Typography>
+      <Typography variant="h6">
         You can change the amino acid display format with the "Amino acid display" button.
       </Typography>
     </Container>
