2228ed5bea9db9a46e905c4d926b1a7e20426f7e
ZsoltCz <zsoltcz2019@gmail.com>
2024-03-04 13:57:55 +0100

    Add script for installing dependencies

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install client side dependencies
+npm --prefix ./NucleicAcidConverter.Client install ./NucleicAcidConverter.Client
+echo "Client side dependencies installed successfully"
+
+# Install server side dependencies
+dotnet
